16031c2fd64dff19078c36f0c7328e93178f051d
Hudyma_Ihor <Hudyma.work@gmail.com>
2022-09-14 20:05:13 +0300

    submitHW

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,8 +3,11 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name';
   
 export function fetchCountries(countryQuery) {
+  const searchParams = new URLSearchParams({
+    fields: 'name,capital,population,flags,languages',
+  })
 
-  return fetch(`${BASE_URL}/${countryQuery}?fields=name.official,capital,population,flags.svg,languages/`)
+  return fetch(`${BASE_URL}/${countryQuery}?${searchParams}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,45 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-import countryInfo from './countryInfo.hbs';
+import countryInfo from './templates/countryInfo.hbs';
+import countryList from './templates/countryList.hbs';
 
+import { create } from 'handlebars';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
+const countryLst = document.querySelector('.country-list');
 const countryInf = document.querySelector('.country-info');
 
 const onInputEl = debounce(() => {
     const countryQuery = inputEl.value.trim();
+    
     if (countryQuery === "") {
+        countryInf.innerHTML = '';
+        countryLst.innerHTML = '';
         return
     }
-
  fetchCountries(countryQuery)
      .then(data => {
-
-//    countryInf.innerHTML = countryInfo(data)
-         
-            console.log(data);
                 if (data.length > 10) {
                     Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+                } else if (data.length >=2) { 
+                    countryLst.innerHTML = countryList(data);
+                    countryInf.innerHTML = '';
+
+                } else {        
+   data[0].languages = Object.values(data[0].languages).join(', ');
+                    countryInf.innerHTML = countryInfo(data);
+                    countryLst.innerHTML = '';
                 }
         })
         .catch(err => {
-            // console.log(err)
             if (err.message === '404') {
-                Notiflix.Notify.info("Oops, there is no country with that name");
-            }
+                Notiflix.Notify.failure("Oops, there is no country with that name");
+            } 
         });
-        console.log(countryQuery)
     }, DEBOUNCE_DELAY);
 
 inputEl.addEventListener('input', onInputEl);
